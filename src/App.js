7dd2f5d19fe686eeb38ea3b2a5416536a640fa10
Michael Guedko <mjgeko@gmail.com>
2023-01-24 13:44:33 -0500

    Add webcam to project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { Box } from '@mui/system';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import PeopleIcon from '@mui/icons-material/People';
 import './App.css';
-import { Avatar } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { Avatar, Button, ButtonBase } from '@mui/material';
+import { useEffect, useRef, useState } from 'react';
+import Webcam from "react-webcam";
 
 function App() {
   const [width,setWidth] = useState(window.innerWidth);
   const breakpoint = 768;
+  
+
   useEffect(() => {
     const handleResizeWindow = () => {
       setWidth(window.innerWidth);
@@ -29,12 +32,16 @@ function App() {
   return (
     <div className="App">
       <Box sx={{height: '80px', width:'100%', bgcolor:'#004D9B', display:'flex',alignItems:'center', justifyContent:'space-around'}}>
-        <NotificationsIcon sx={{color:'white', height:'40px', width:'40px'}}/>
+        <ButtonBase>
+          <NotificationsIcon  sx={{color:'white', height:'40px', width:'40px'}}/>
+        </ButtonBase>
         <Avatar />
-        <PeopleIcon sx={{color:'white', height:'40px', width:'40px'}}/>
+        <ButtonBase>
+          <PeopleIcon sx={{color:'white', height:'40px', width:'40px'}}/>
+        </ButtonBase>
       </Box>
+      <Webcam height={500}/>
       <Box sx={{height: '80px',width:'100%', bgcolor:'#004D9B'}}>
-          
       </Box>
     </div>
   );
