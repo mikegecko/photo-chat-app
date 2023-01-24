8fa48fbb2ccce210fa550b2a6e2b0de4fac276dc
Michael Guedko <mjgeko@gmail.com>
2023-01-24 12:44:54 -0500

    Add app skeleton

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Box } from '@mui/system';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Box sx={{height: '80px',width:'100%', bgcolor:'#004D9B'}}>
+        
+      </Box>
+      <Box sx={{height: '80px',width:'100%', bgcolor:'#004D9B'}}>
+          
+      </Box>
     </div>
   );
 }
