d8eb7bc3f70d0f5810aa9490b88a7efac5c5672c
Michael Guedko <mjgeko@gmail.com>
2023-01-25 13:17:32 -0500

    Add styling to login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,13 +2,14 @@ import { LoadingButton } from "@mui/lab";
 import { Box } from "@mui/system";
 
 
-export default function Login ({loading, hidden}) {
+export default function Login ({loading, hidden, loginHandler}) {
     if(hidden){
         return(<></>);
     }
     return(
-        <Box>
-            <LoadingButton variant="outlined" loading={loading}>Sign-In with Google™</LoadingButton>
+        <Box sx={{display:'flex', flexDirection:'column',justifyContent:'center', alignItems:'center', position:'absolute', width: '100%', height: '100%', bgcolor: '#1F1F1F', zIndex:10}}>
+            <Box sx={{padding:'3rem', color:'white', fontWeight:'bold',fontSize:64}}>PicFlo</Box>
+            <LoadingButton onClick={loginHandler} variant="outlined" loading={loading}>Sign-In with Google™</LoadingButton>
         </Box>
     )
 }
