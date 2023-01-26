7111331d7bf2e9d2e51af2cf83253ff0b9aa2a00
Michael Guedko <mjgeko@gmail.com>
2023-01-25 23:50:02 -0500

    Add some gradient and styling to login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,34 @@
 import { LoadingButton } from "@mui/lab";
+import { createTheme, ThemeProvider } from "@mui/material";
 import { Box } from "@mui/system";
 
 
 export default function Login ({loading, hidden, loginHandler}) {
+    const theme = createTheme({
+        status: {
+            danger: '#e53e3e',
+        },
+        palette: {
+            primary: {
+                main: '#3e9dfc',
+                darker: '#1f1f1f',
+              },
+              neutral: {
+                main: '#64748B',
+                contrastText: '#fff',
+              },
+        }
+    })
     if(hidden){
         return(<></>);
     }
     return(
-        <Box sx={{display:'flex', flexDirection:'column',justifyContent:'center', alignItems:'center', position:'absolute', width: '100%', height: '100%', bgcolor: '#1F1F1F', zIndex:10}}>
+        <ThemeProvider theme={theme}>
+        <Box sx={{display:'flex', flexDirection:'column',justifyContent:'center', alignItems:'center', position:'absolute', width: '100%', height: '100%', background:'linear-gradient(180deg, rgba(31,31,31,1) 9%, rgba(0,73,147,1) 52%, rgba(65,0,121,1) 100%)', zIndex:10}}>
             <Box sx={{padding:'3rem', color:'white', fontWeight:'bold',fontSize:64}}>PicFlo</Box>
-            <LoadingButton onClick={loginHandler} variant="outlined" loading={loading}>Sign-In with Google™</LoadingButton>
+            <LoadingButton color="primary" onClick={loginHandler} variant="outlined" loading={loading}>Sign-In with Google™</LoadingButton>
         </Box>
+        </ThemeProvider>
+        
     )
 }
