b5985863846933f946a4e8c9dedbf2167c24bca9
Michael Guedko <mjgeko@gmail.com>
2023-02-01 12:20:06 -0500

    Add header

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,11 +1,12 @@
+import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 
 
 export default function Settings(props){
 
     return(
-        <Box>
-            
+        <Box sx={{display:'flex',height:'100%', flexDirection:'column', color:'white' }}>
+            <Typography variant='h4' sx={{paddingTop:'10px'}}>Settings</Typography>
         </Box>
     )
 }
