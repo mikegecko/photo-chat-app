06379c89e049f5bbfdbc10a7655fea936db0bf49
Michael Guedko <mjgeko@gmail.com>
2023-02-01 14:27:56 -0500

    Add placeholder stuff

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,12 +1,20 @@
+import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 
-
 export default function Notifications() {
-
-
-    return(
-        <Box>
-            
-        </Box>
-    )
+    //This is all placeholder
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        height: "100%",
+        flexDirection: "column",
+        color: "white",
+      }}
+    >
+      <Typography variant="h4" sx={{ paddingTop: "10px" }}>
+        Notifications
+      </Typography>
+    </Box>
+  );
 }
