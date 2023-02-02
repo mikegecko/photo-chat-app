85ef8cfb88b39fe0f3ab89cfce7ecab9bcabbca7
Michael Guedko <mjgeko@gmail.com>
2023-02-02 00:03:24 -0500

    Add theme to Notifications

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,20 +1,23 @@
-import { Typography } from "@mui/material";
+import { ThemeProvider, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { theme } from "../theme/theme";
 
 export default function Notifications() {
-    //This is all placeholder
+  //This is all placeholder
   return (
-    <Box
-      sx={{
-        display: "flex",
-        height: "100%",
-        flexDirection: "column",
-        color: "white",
-      }}
-    >
-      <Typography variant="h4" sx={{ paddingTop: "10px" }}>
-        Notifications
-      </Typography>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          display: "flex",
+          height: "100%",
+          flexDirection: "column",
+          color: "white",
+        }}
+      >
+        <Typography variant="h4" sx={{ paddingTop: "10px" }}>
+          Notifications
+        </Typography>
+      </Box>
+    </ThemeProvider>
   );
 }
