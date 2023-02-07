84a6e3dfb07c938e467f374bba177e755ede3ffe
Michael Guedko <mjgeko@gmail.com>
2023-02-07 18:27:22 -0500

    Dynamically assign message style

--- src/components/StyledMessage.jsx
+++ src/components/StyledMessage.jsx
@@ -3,16 +3,21 @@ import { Box } from "@mui/system";
 
 export default function StyledMessage (props) {
 
-    const senderStyle = {}
-    const receiverStyle = {
-  
-    }
+    const senderStyle = {display: 'flex', justifyContent:'flex-start', alignSelf:'flex-end', bgcolor: '#002b5a', flexDirection: 'row', margin: '8px', padding: '1rem', borderRadius: '.7rem .7rem 0 .7rem'}
+    const receiverStyle = {display: 'flex', justifyContent:'flex-start', alignSelf:'flex-start', bgcolor: '#454545', flexDirection: 'row', margin: '8px', padding: '1rem', borderRadius: '.7rem .7rem .7rem 0'}
     const styleMessage = () => {
-  
+        //Message is from user
+        if(props.userID === props.message.sender){
+            return(senderStyle);
+        }
+        //Message is from friend
+        else{
+            return(receiverStyle);
+        }
     }
 
     return(
-        <Box key={props.id} sx={{display: 'flex', justifyContent:'flex-start', alignSelf:'flex-end', bgcolor: '#454545', flexDirection: 'row', margin: '8px', padding: '1rem', borderRadius: '.7rem .7rem 0 .7rem'}}>
+        <Box key={props.id} sx={() => styleMessage()}>
             {props.message.content}
         </Box>
     )
