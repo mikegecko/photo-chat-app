c83548e976ec51a6e53cbbe9d078e802380d4580
Michael Guedko <mjgeko@gmail.com>
2023-02-09 18:42:33 -0500

    Add dialog for adding Friends     - dialog with text input for friend code     - buttons for sending friend request or open qr code scanner

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,11 +1,19 @@
 import {
+  Button,
+  ButtonBase,
   Checkbox,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   Divider,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  TextField,
   ThemeProvider,
   Typography,
 } from "@mui/material";
@@ -17,11 +25,30 @@ import { useState } from "react";
 
 export default function Friends(props) {
   const [checked, setChecked] = useState(false);
+  const [addFriendDialog, setAddFriendDialog] = useState(false);
+  const [friendCode, setFriendCode] = useState(null);
 
   const handleToggle = (e) => {
     setChecked(!checked);
   };
 
+  const handleAddFriendEvent = (e) => {
+    //Send a friend request to another user
+    /*
+      1. Open dialog for entering friend code or scanning qr code
+      2. On submit check if valid friend code 
+        - add error if fail
+      3. change friend DB to include a new friend request
+    */
+   setAddFriendDialog(true);
+  }
+
+  const handleFriendCodeInput = (e) => {
+    setFriendCode(e.target.value);
+  }
+  const sendFriendRequest = () => {
+    console.log(friendCode);
+  }
   if (props.isSending) {
     return (
       <ThemeProvider theme={props.theme}>
@@ -79,13 +106,25 @@ export default function Friends(props) {
             flexDirection: "column",
           }}
         >
+          <Dialog open={addFriendDialog} onClose={() => setAddFriendDialog(false)}>
+            <DialogTitle>Send Friend Request</DialogTitle>
+            <DialogContent>
+              <DialogContentText>Enter friend code or scan QR code</DialogContentText>
+              <TextField onChange={handleFriendCodeInput} autoFocus margin="dense" id="friendcode" label="Friend Code" type="text" fullWidth variant="standard" />
+            </DialogContent>
+            <DialogActions sx={{gap: '8px'}}>
+              <Button variant="text" onClick={sendFriendRequest} >Add Friend</Button>
+              <Button variant="text" >QR Code</Button>
+              <Button variant="outlined" onClick={() => setAddFriendDialog(false)}>Cancel</Button>
+            </DialogActions>
+          </Dialog>
           <Typography variant="h4" sx={{ paddingTop: "10px", paddingBottom: "10px"}}>
             Friends
           </Typography>
           <List disablePadding sx={{ width: "100%", fontFamily: "Roboto" }}>
             <Divider variant="fullWidth" component="li" />
             <ListItem disablePadding secondaryAction={<PersonAddIcon sx={{display:'flex'}} />}>
-              <ListItemButton>
+              <ListItemButton onClick={handleAddFriendEvent}>
                 <ListItemText primary="Add Friend" />
               </ListItemButton>
             </ListItem>
