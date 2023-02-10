6696b925ed20373010fc87fa2f3f673e942ac832
Michael Guedko <mjgeko@gmail.com>
2023-02-10 17:07:22 -0500

    Add temp Notification

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,6 +1,8 @@
-import { ThemeProvider, Typography } from "@mui/material";
+import { Divider, IconButton, List, ListItem, ListItemButton, ListItemText, ThemeProvider, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { theme } from "../theme/theme";
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function Notifications(props) {
   //This is all placeholder
@@ -21,6 +23,23 @@ export default function Notifications(props) {
         <Typography variant="h4" sx={{ paddingTop: "10px", paddingBottom: "10px" }}>
           Notifications
         </Typography>
+        <Divider variant="fullWidth" />
+        <List disablePadding >
+          {props.userData.friends.map((el,index) => {
+            if(!el.accepted){
+              return(
+                <>
+                <ListItem disablePadding secondaryAction={<IconButton><CloseIcon sx={{opacity: '100%', color:''}} /></IconButton>} >
+                  <ListItemButton >
+                    <ListItemText primary={el.name + ' sent you a friend request.'} />
+                  </ListItemButton>
+                </ListItem>
+                <Divider variant="fullWidth" component="li" />
+                </>
+                )
+            }
+          })}
+        </List>
       </Box>
     </ThemeProvider>
   );
