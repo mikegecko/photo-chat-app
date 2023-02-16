eeda6a4f15ddb7f168f8776b5ea5bea7e98079ff
Michael Guedko <mjgeko@gmail.com>
2023-02-16 13:23:22 -0500

    Add check for camera

--- src/components/QRScan.jsx
+++ src/components/QRScan.jsx
@@ -1,4 +1,4 @@
-import { ButtonBase } from "@mui/material";
+import { ButtonBase, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import QrScanner from "qr-scanner";
 import { useEffect, useRef, useState } from "react";
@@ -7,6 +7,7 @@ import CloseIcon from "@mui/icons-material/Close";
 export default function QRScan(props) {
   const vidRef = useRef(null);
   const [qrScanner, setQrScanner] = useState();
+  const [hasCamera, setHasCamera] = useState();
 
   const scanErrorHandler = (err) => {
     console.error(err);
@@ -25,7 +26,16 @@ export default function QRScan(props) {
       }
     );
     setQrScanner(scan);
+    async function cameraCheck() {
+      const result = await QrScanner.hasCamera().then();
+      return result;
+    }
     scan.start();
+    if (!cameraCheck()) {
+      setHasCamera(false);
+    } else {
+      setHasCamera(true);
+    }
     return () => {
       scan.stop();
     };
@@ -38,31 +48,61 @@ export default function QRScan(props) {
     console.log(qrScanner);
   }, [qrScanner]);
 
-  return (
-    <Box
-      sx={{
+  if (hasCamera) {
+    return <Box
+        sx={{
         position: "absolute",
         display: "flex",
         justifyContent: "center",
-        top: '80px',
-        height: "calc(100% - 80px)",
+        alignItems: 'center',
+        top: "80px",
+        height: "calc(100% - 160px)",
+        width: '100%',
         backgroundColor: "#1f1f1f",
+        opacity: '100%',
         zIndex: 1000,
-      }}
-    >
-      <ButtonBase
-        onClick={props.closeScannerEvent}
+      }}>
+        <ButtonBase
+          onClick={props.closeScannerEvent}
+          sx={{
+            position: "absolute",
+            top: "25px",
+            left: "20px",
+            opacity: "54%",
+            zIndex: 1005,
+          }}
+        >
+          <CloseIcon sx={{ color: "white", height: "30px", width: "30px" }} />
+        </ButtonBase>
+        <Typography variant="h4" sx={{top: '50%'}}>No Camera</Typography>
+    </Box>;
+  } else {
+    return (
+      <Box
         sx={{
           position: "absolute",
-          top: "25px",
-          left: "20px",
-          opacity: "54%",
-          zIndex: 5,
+          display: "flex",
+          justifyContent: "center",
+          top: "80px",
+          height: "calc(100% - 80px)",
+          backgroundColor: "#1f1f1f",
+          zIndex: 1000,
         }}
       >
-        <CloseIcon sx={{ color: "white", height: "30px", width: "30px" }} />
-      </ButtonBase>
-      <video ref={vidRef}></video>
-    </Box>
-  );
+        <ButtonBase
+          onClick={props.closeScannerEvent}
+          sx={{
+            position: "absolute",
+            top: "25px",
+            left: "20px",
+            opacity: "54%",
+            zIndex: 1005,
+          }}
+        >
+          <CloseIcon sx={{ color: "white", height: "30px", width: "30px" }} />
+        </ButtonBase>
+        <video ref={vidRef}></video>
+      </Box>
+    );
+  }
 }
