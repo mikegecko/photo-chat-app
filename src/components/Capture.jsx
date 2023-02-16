fc59d2c493b5a0abe0918ba764459444657446b3
Michael Guedko <mjgeko@gmail.com>
2023-02-16 14:46:33 -0500

    Add desktop view

--- src/components/Capture.jsx
+++ src/components/Capture.jsx
@@ -2,18 +2,26 @@ import { ButtonBase } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import { Box } from "@mui/system";
 
-export default function Capture({ capture, closeEvent }) {
+export default function Capture({ capture, closeEvent, mobileView }) {
+  const mobileStyle = {
+    position: "absolute",
+    top: "105px",
+    left: "20px",
+    opacity: "54%",
+    zIndex: 5,
+  }
+  const desktopStyle = {
+    position: "absolute",
+    top: "105px",
+    left: "calc(30% + 20px)",
+    opacity: "54%",
+    zIndex: 5,
+  }
   return (
     <Box>
       <ButtonBase
         onClick={closeEvent}
-        sx={{
-          position: "absolute",
-          top: "105px",
-          left: "20px",
-          opacity: "54%",
-          zIndex: 5,
-        }}
+        sx={mobileView ? mobileStyle : desktopStyle}
       >
         <CloseIcon sx={{ color: "white", height: "30px", width: "30px" }} />
       </ButtonBase>
