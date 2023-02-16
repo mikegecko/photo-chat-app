fa0ce11cb47cc63be54683111ed343fe54bbcf69
Michael Guedko <mjgeko@gmail.com>
2023-02-16 12:45:44 -0500

    Add QrScanner     - Managed to get scanner working     - updates friendcode value when scan completes     - crash if user is already focused friendcode field in dialog     - handle error if no camera detected     - bug: friendcode overlaps placeholder text

--- src/components/QRScan.jsx
+++ src/components/QRScan.jsx
@@ -1,27 +1,68 @@
+import { ButtonBase } from "@mui/material";
 import { Box } from "@mui/system";
 import QrScanner from "qr-scanner";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
 
-export default function QRScan (props) {
-    const vidRef = useRef();
-    const qrScanner = new QrScanner(vidRef.current, result => console.log(result),{preferredCamera: 'environment',returnDetailedScanResult: true});
+export default function QRScan(props) {
+  const vidRef = useRef(null);
+  const [qrScanner, setQrScanner] = useState();
 
-    const scanErrorHandler = (err) => {
+  const scanErrorHandler = (err) => {
+    console.error(err);
+  };
 
-    }
+  useEffect(() => {
+    const scan = new QrScanner(
+      vidRef.current,
+      (result) => props.setStateOfFriendCode(result.data),
+      {
+        preferredCamera: "environment",
+        returnDetailedScanResult: true,
+        onDecodeError: (error) => scanErrorHandler(error),
+        highlightScanRegion: true,
+        highlightCodeOutline: true,
+      }
+    );
+    setQrScanner(scan);
+    scan.start();
+    return () => {
+      scan.stop();
+    };
+  }, []);
+  useEffect(() => {
+    // qrScanner.start();
+    // return () => {
+    //     qrScanner.stop();
+    // }
+    console.log(qrScanner);
+  }, [qrScanner]);
 
-    
-
-    useEffect(() => {
-        qrScanner.start();
-        return () => {
-            qrScanner.stop();
-        }
-    }, [])
-
-    return(
-        <Box>
-            <video ref={vidRef}></video>
-        </Box>
-    )
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        display: "flex",
+        justifyContent: "center",
+        top: '80px',
+        height: "calc(100% - 80px)",
+        backgroundColor: "#1f1f1f",
+        zIndex: 1000,
+      }}
+    >
+      <ButtonBase
+        onClick={props.closeScannerEvent}
+        sx={{
+          position: "absolute",
+          top: "25px",
+          left: "20px",
+          opacity: "54%",
+          zIndex: 5,
+        }}
+      >
+        <CloseIcon sx={{ color: "white", height: "30px", width: "30px" }} />
+      </ButtonBase>
+      <video ref={vidRef}></video>
+    </Box>
+  );
 }
