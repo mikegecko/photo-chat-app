339e58a5ff33379494c11c740c1cf677041d4a9c
Michael Guedko <mjgeko@gmail.com>
2023-02-17 23:27:31 -0500

    Started working on press to view interaction

--- src/components/StyledImageMessage.jsx
+++ src/components/StyledImageMessage.jsx
@@ -1,26 +1,71 @@
 /** @jsxImportSource @emotion/react */
 import { Box } from "@mui/system";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { css } from '@emotion/react'
-
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import { ButtonBase, Typography } from "@mui/material";
 
 export default function StyledImageMessage (props) {
-
-    const senderStyle = {width: '80%' , display: 'flex', justifyContent:'center', alignItems: 'center', alignSelf:'flex-end', bgcolor: '#002b5a', flexDirection: 'row', margin: '8px', padding: '4px', borderRadius: '.7rem .7rem 0 .7rem'}
+    const [view, setView] = useState(false);
+    const senderStyle = { display: 'flex', justifyContent:'center', alignItems: 'center', alignSelf:'flex-end', bgcolor: '#002b5a', flexDirection: 'row', margin: '8px', padding: '4px', borderRadius: '.7rem .7rem 0 .7rem'}
     const receiverStyle = {display: 'flex', justifyContent:'center', alignItems: 'center', alignSelf:'flex-start', bgcolor: '#454545', flexDirection: 'row', margin: '8px', padding: '4px', borderRadius: '.7rem .7rem .7rem 0'}
+    const viewStyle = { display: 'flex', gap: '1rem', justifyContent:'center', alignItems: 'center', alignSelf:'flex-end', bgcolor: '#002b5a', flexDirection: 'row', margin: '8px', padding: '.5rem 1rem .5rem 1rem', borderRadius: '.7rem .7rem 0 .7rem'}
+
     const styleMessage = () => {
         //Message is from user
         if(props.userID === props.message.sender){
-            return(senderStyle);
+            //return(senderStyle);
+            return(viewStyle);
         }
         //Message is from friend
         else{
             return(receiverStyle);
         }
     }
+    const viewContent = () => {
+        if(!view){
+            return(
+                <>
+                    <VisibilityIcon />
+                    <Typography>
+                        Press to View
+                    </Typography>
+                </>
+            )
+        }else{
+            return(
+                <>
+                    <VisibilityOutlinedIcon />
+                    <Typography>
+                        Viewed
+                    </Typography>
+                </>
+            )
+        }
+    }
+    const viewEvent = e => {
+        setView(!view);
+    }
     useEffect(() => {
         //console.log(props.message)
+        //setView(true);
     }, [])
+
+    if(true){
+        return(
+            
+            <Box key={props.id} sx={() => styleMessage()}>
+                <ButtonBase onClick={viewEvent} sx={() => styleMessage()}>
+                {/* {view ? <VisibilityIcon /> : <VisibilityOutlinedIcon />}
+                {view ? 'Press To View' : 'Viewed'} */}
+                {viewContent()}
+                </ButtonBase>
+            </Box>
+            
+        )
+    }
+
     return(
         <Box key={props.id} sx={() => styleMessage()}>
             <img css={css`
