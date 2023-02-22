159dc8440ec72461cd13aaa2cae2150591ac3ca6
Michael Guedko <mjgeko@gmail.com>
2023-02-22 18:52:47 -0500

    Fix issue

--- src/helpers/apiKeys.jsx
+++ src/helpers/apiKeys.jsx
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
@@ -8,5 +8,3 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
-
-export default firebaseConfig;
